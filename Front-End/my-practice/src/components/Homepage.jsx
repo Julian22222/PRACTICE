9f7d65f43029e644eca41f7d8be344c1ebc89cd7
Julian22222 <julik.golovenj@gmail.com>
2023-02-16 15:08:00 +0000

    main logo added

--- Front-End/my-practice/src/components/Homepage.jsx
+++ Front-End/my-practice/src/components/Homepage.jsx
@@ -6,6 +6,12 @@ const Homepage = () => {
       <h1 className="Header-colour">
         Come and taste our amazing Wines from all around the world!
       </h1>
+      <img
+        src="https://www.winemag.com/wp-content/themes/TrellisFoundation-child/assets/img/toplists_header.jpg"
+        alt="wines are standing in a row"
+        width="1680"
+        height="300"
+      />
       <h1 className="Header">Best Wine Shop</h1>
     </div>
   );
