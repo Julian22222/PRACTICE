6d0b3c30fb165b6092fd77ebb06cad35fe94822c
Julian22222 <julik.golovenj@gmail.com>
2023-02-16 16:17:07 +0000

    host

---
+++ MongoDB/routes/movie-routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  getMovies,
+  getMovie,
+  deleteMovie,
+  addMovie,
+  updateMovie,
+} = require("../controllers/movie-controller");
+
+router.get("/movies", getMovies);
+// db - hold the data from Database in successesful connection to Database
+router.get("/movies/:id", getMovie);
+router.delete("/movies/:id", deleteMovie);
+router.post("/movies", addMovie);
+router.patch("/movies/:id", updateMovie);
+
+module.exports = router;
---
+++ MongoDB/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Movie = require("./models/movie");
+const movieRoutes = require("./routes/movie-routes");
+
+const PORT = 3000;
+// this URL we use without hosting
+const URL = "mongodb://localhost:27017/moviebox";
+// const URL =
+//   "mongodb+srv://julikgolovenj:Julik123@cluster0.rmfxbzb.mongodb.net/moviebox?retryWrites=true&w=majority";
+// localhost :27017from mongodb compass / moviebox -our Database
+
+const app = express();
+
+app.use(express.json());
+app.use(movieRoutes);
+
+mongoose
+  .connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(`DB connection error: ${err}`);
+  });
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`Server is listening on port ${PORT}`);
+});
