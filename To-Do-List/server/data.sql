961749cc8a58a73f966135b43cd2ad3d8ec527ae
Julian22222 <julik.golovenj@gmail.com>
2023-07-03 21:02:15 +0100

    changes

--- To-Do-List/server/data.sql
+++ To-Do-List/server/data.sql
@@ -6,19 +6,19 @@ CREATE DATABASE todo_app;
 
 CREATE TABLE todos(
     todo_id SERIAL PRIMARY KEY,
-    user_email VARCHAR(255),
+    userEmail VARCHAR(255),
     title VARCHAR(30),
     progress INT,
     date VARCHAR(300)
 );
 
 CREATE TABLE users(
-    email SERIAL PRIMARY KEY,
+    userEmail SERIAL PRIMARY KEY,
     hashed_password VARCHAR(255)
 );
 
 INSERT INTO todos
-(user_email, title, progress, date)
+(userEmail, title, progress, date)
 VALUES('julian@test.com', 'First todo', 10, 'Thu March 2 2023 19:15:35 GMT+0400 (Gulf Standard Time)');
 
 -- INSERT INTO users
