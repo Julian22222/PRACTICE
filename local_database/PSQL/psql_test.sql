7c4e3f38b9eabad931f31a01e34b1f26d87b182e
Julian22222 <julik.golovenj@gmail.com>
2023-10-10 18:22:56 +0100

    mySQL

---
+++ local_database/PSQL/psql_test.sql
@@ -0,0 +1,10 @@
+-- DROP DATABASE IF EXISTS test_database;
+
+-- CREATE DATABASE test_database;
+
+\c test_database;
+
+-- \dt
+-- \d users;
+
+SELECT * FROM users;
---
+++ local_database/SQLite/mysql.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE employers(employer_id INTEGER NOT NULL PRIMARY KEY,
+-- employer_name VARCHAR(20) NOT NULL, email VARCHAR(50) NOT NULL);
+
+-- INSERT INTO employers(employer_id,employer_name,email)
+-- VALUES(3,"Dan","dan@test.com");
+
+SELECT * FROM employers;
