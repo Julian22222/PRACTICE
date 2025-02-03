65cf1824ca34256fe7ee7deccdf5e0c2c37cc9d5
Julian22222 <julik.golovenj@gmail.com>
2025-02-03 23:18:13 +0000

    dockerfile added

--- MySQL-database/server.js
+++ MySQL-database/server.js
@@ -1,6 +1,6 @@
 const app = require("./app"); //Import app.js file here
 const PORT = 9080;
 
-app.listen(PORT, (err) => {
+app.listen(PORT, "0.0.0.0", (err) => {
   err ? console.log(err) : console.log(`Server is listening on PORT ${PORT}`);
 });
