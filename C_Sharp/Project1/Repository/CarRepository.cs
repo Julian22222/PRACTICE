44964cd141483de4ac5fbfe56bfd71c02fefb4f8
Julian22222 <julik.golovenj@gmail.com>
2025-05-15 22:41:05 +0100

    changes

--- C_Sharp/Project1/Repository/CarRepository.cs
+++ C_Sharp/Project1/Repository/CarRepository.cs
@@ -8,35 +8,6 @@ namespace Project1.Repository
     public class CarRepository  : ICarRepository  //inherit from ICarRepository (interface)
     {
         
-        // public async Task<List<Car>> GetAllCars(){
-        //     return Allcars();
-        // //Local application data storage, dont use DB here 
-
-        // }
-
-
-        // public async Task<Car> GetCarById(int id){
-        //     return Allcars().Where(x=>x.Id==id).FirstOrDefault();
-        // }
-
-        
-
-        //Local application data storage, dont use DB here 
-        // private List<Car> Allcars(){
-        //     return new List<Car>(){
-        //     new Car(){Id=1,Name="Toyota",Price=10000,Year=DateTime.Now,FuelType="Petrol"},
-        //     new Car(){Id=2,Name="Honda",Price=20000,Year=DateTime.Now,FuelType="Diesel"},
-        //     new Car(){Id=3,Name="Suzuki",Price=30000,Year=DateTime.Now,FuelType="Petrol"},
-        //     new Car(){Id=4,Name="BMW",Price=40000,Year=DateTime.Now,FuelType="Diesel"},
-        //     new Car(){Id=5,Name="Audi",Price=50000,Year=DateTime.Now,FuelType="Petrol"},
-        //     new Car(){Id=6,Name="Mercedes",Price=60000,Year=DateTime.Now,FuelType="Diesel"},
-        //     new Car(){Id=7,Name="Ford",Price=70000,Year=DateTime.Now,FuelType="Petrol"},
-        //     new Car(){Id=8,Name="Chevrolet",Price=80000,Year=DateTime.Now,FuelType="Diesel"},
-        //     new Car(){Id=9,Name="Ferrari",Price=90000,Year=DateTime.Now,FuelType="Petrol"},
-        //     new Car(){Id=10,Name="Lamborghini",Price=100000,Year=DateTime.Now,FuelType="Diesel"}       
-        //     };
-        // }
-
     public List<Car> GetAllCars()
     // public IEnumerable<Car> GetAllCars()
     {
